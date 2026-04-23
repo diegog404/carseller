8c3f05cb279639674080ac12e69c2cced4d88c3d
Diego Gomes <diegogns18@outlook.com>
2026-04-23 13:49:07 -0300

    Vehicle CRUD

--- carseller/Controllers/VehiclesController.cs
+++ carseller/Controllers/VehiclesController.cs
@@ -1,7 +1,9 @@
 ﻿using carseller.Models;
 using carseller.Models.ViewModels;
 using carseller.Services;
+using carseller.Services.Exceptions;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace carseller.Controllers
 {
@@ -35,6 +37,89 @@ namespace carseller.Controllers
         { 
             await _vehicleService.InsertAsync(vehicle);
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided." });
+            }
+
+            var obj = await _vehicleService.FindByIdAsync(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not found." });
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _vehicleService.RemoveAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided." });
+            }
+
+            var obj = await _vehicleService.FindByIdAsync(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not found." });
+            }
+
+            return View(obj);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided." });
+            }
+
+            var obj = await _vehicleService.FindByIdAsync(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
+            }
+
+            List<Company> companies = await _companyService.FindAllAsync();
+
+            VehicleFormViewModel viewModel = new VehicleFormViewModel { Vehicle = obj, Companies = companies };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Vehicle vehicle)
+        {
+            if (id != vehicle.Id)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
+            }
+
+            try
+            {
+                await _vehicleService.UpdateAsync(vehicle);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+            catch (DbConcurrencyException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
         }
     }
 }
--- carseller/Services/VehicleService.cs
+++ carseller/Services/VehicleService.cs
@@ -1,5 +1,6 @@
 ﻿using carseller.Data;
 using carseller.Models;
+using carseller.Services.Exceptions;
 using Microsoft.EntityFrameworkCore;
 
 namespace carseller.Services
@@ -22,6 +23,39 @@ namespace carseller.Services
         {
             _context.Add(obj);
             await _context.SaveChangesAsync();
+        }
+
+        public async Task<Vehicle> FindByIdAsync(int id)
+        {
+            return await _context.Vehicle.Include(obj => obj.Company).FirstOrDefaultAsync(obj => obj.Id == id);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            var obj = await _context.Vehicle.FindAsync(id);
+            _context.Vehicle.Remove(obj);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Vehicle obj)
+        {
+            bool hasAny = await _context.Vehicle.AnyAsync(x => x.Id == obj.Id);
+
+
+            if (!hasAny)
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
